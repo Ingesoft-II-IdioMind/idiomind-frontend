dfd84bea35f3bcf783e4afb5bd3bd39a63b7ba15
Jefferson Duvan Ramirez <jeramirezca@unal.edu.co>
2024-04-03 21:01:54 -0500

    correction 2 login

--- src/utils/continue-with-social-auth.ts
+++ src/utils/continue-with-social-auth.ts
@@ -5,11 +5,13 @@ export default async function continueWithSocialAuth(
 	redirect: string
 ) {
 	try {
+		const redirectUrl = process.env.NEXT_PUBLIC_REDIRECT_URL;
+		const newRedirectUrl = redirectUrl ? redirectUrl.slice(0, -1) : '';
 		const url = `${
 			process.env.NEXT_PUBLIC_HOST
 		}/api/o/${provider}/?redirect_uri=${
 			process.env.NODE_ENV === 'production'
-				? process.env.NEXT_PUBLIC_REDIRECT_URL
+				? newRedirectUrl
 				: 'http://localhost:3000'
 		}`;
 
